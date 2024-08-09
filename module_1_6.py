c3afe531c2aa3a41abef3d31b1884173140c2739
Alex Karlov <140255198+Alex-ex999@users.noreply.github.com>
2024-08-09 11:59:47 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -6,7 +6,7 @@ my_dict.update({'Nick' : 1999,
                 'Anna': 2009})
 my_dict.pop('Nick')
 print(my_dict)
-my_set = {9,'bycicle', 56.98,9,'bycicle',13,'Olimpic'}
+my_set = {9,'bicycle', 56.98,9,'bicycle',13,'Olimpic'}
 my_set.update((4,8))
 my_set.discard(56.98)
 print(my_set)
